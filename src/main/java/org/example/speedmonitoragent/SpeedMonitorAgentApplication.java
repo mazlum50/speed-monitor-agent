81affba81602c50cb385431d7521588a5da4642c
mazlumismail <mazlumism50@gmail.com>
2026-09-10 23:38:43 +0200

    Initialer Commit: Projektstruktur mit Spring Boot, Batch und H2 eingerichtet

---
+++ src/main/java/org/example/speedmonitoragent/SpeedMonitorAgentApplication.java
@@ -0,0 +1,13 @@
+package org.example.speedmonitoragent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeedMonitorAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeedMonitorAgentApplication.class, args);
+	}
+
+}
